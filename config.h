d0ca055fc0d1f3b3f594feec17c1d24282925ed1
chamber <cjk1982@gmail.com>
2015-11-11 22:12:14 +0000

    20151111

--- config.h
+++ config.h
@@ -41,10 +41,10 @@ static const Bool resizehints = True; /* True means respect size hints in tiled 
 
 static const Layout layouts[] = {
 	/* symbol   arrange function */
+	{ "ü",      bstack },
 	{ "þ",      tile },    /* first entry is default */
 	{ "ý",      NULL },    /* no layout function means floating behavior */
 	{ "ÿ",      monocle },
-	{ "ü",      bstack },
 	{ "û",      bstackhoriz },
 };
 
